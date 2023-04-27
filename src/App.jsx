8bae3f7e25af895cd8b2233b53443b0ee6b581ad
Piyush <yadavpiyush222@gmail.com>
2023-04-27 18:32:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import React from "react"
+import './App.css'
+import ProblemPage from "./pages/problemPage/problemPage"
+import ProblemsListPage from "./pages/problemsListPage/problemsListPage"
+import SignInPage from "./pages/signinpage/signinPage"
+import SignUpPage from "./pages/signupPage/signupPage"
+import ExplorePage from "./pages/explorePage/explorePage"
+
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: ("/"),
+        element: <ExplorePage />,
+    },
+    {
+        path: ("/signin"),
+        element: <SignInPage />,
+    },
+    {
+        path: "/signup",
+        element: <SignUpPage />,
+    },
+    {
+        path: "/problems",
+        element: <ProblemsListPage />,
+    },
+    {
+        path: "/:id",
+        element: <ProblemPage />,
+    },
+
+]);
+
+function App() {
+
+    /* Add routing here, routes look like -
+       /login - Login page
+       /signup - Signup page
+       /problemset/all/ - All problems (see problems array above)
+       /problems/:problem_slug - A single problem page
+     */
+
+    return (
+
+        <RouterProvider router={router} />
+
+    )
+}
+
+// A demo component
+
+export default App
